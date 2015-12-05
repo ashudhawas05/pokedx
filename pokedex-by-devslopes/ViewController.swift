2102f92e9bd6d8dcecc50c7d402bce19e15c350a
ashudhawas05 <ashudhawas05@gmail.com>
2015-12-05 13:11:29 +0530

    Initial Commit

---
+++ pokedex-by-devslopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-devslopes
+//
+//  Created by ABB'S Infotech on 12/5/15.
+//  Copyright © 2015 ABBS COMPUTERS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
